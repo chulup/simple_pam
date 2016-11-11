4b4ae8cf6861f0bf71805a2584d9540076d6bdf8
Michael Shulbaev <chulupey@gmail.com>
2016-11-11 11:06:34 +0600

    Move to pam_prompt

--- src/simple_pam.cpp
+++ src/simple_pam.cpp
@@ -1,8 +1,9 @@
+#include <iostream>
 #include <random>
-#include <stdio.h>
 #include <string>
 
 #include <security/pam_appl.h>
+#include <security/pam_ext.h>
 #include <security/pam_modules.h>
 
 //#include "authenticator.h"
@@ -11,21 +12,6 @@
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
     return PAM_SUCCESS;
 }
-
-/* this function is ripped from pam_unix/support.c, it lets us do IO via PAM */
-int converse( pam_handle_t *pamh, int nargs, struct pam_message **message, struct pam_response **response ) {
-    int retval ;
-    struct pam_conv *conv ;
-
-    retval = pam_get_item( pamh, PAM_CONV, (const void **) &conv ) ; 
-    if( retval==PAM_SUCCESS ) {
-        retval = conv->conv( nargs, (const struct pam_message **) message, response, conv->appdata_ptr ) ;
-    }
-
-    return retval ;
-}
-
-#define LOG(fmt, ...) if(!silent) printf(fmt, ##__VA_ARGS__)
 
 /* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
@@ -39,33 +25,24 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
         return retval;
     }
     
-    LOG("Hello, %s", username);
-
     {
-        /* generating a random one-time code */
         std::string request = std::to_string(std::random_device{} ());
-        char prompt[100];
-        snprintf(prompt, sizeof(prompt), "Type that number back: %s\n", request.c_str());
-        struct pam_message msg, *pmsg[1];
-        struct pam_response *resp = NULL;
+        char *response = NULL;
 
-        msg.msg_style = PAM_PROMPT_ECHO_ON;
-        msg.msg = prompt;
-        pmsg[0] = &msg;
-
-        retval = converse(pamh, 1, pmsg, &resp);
+        retval = pam_prompt(pamh,
+            PAM_PROMPT_ECHO_ON,
+            &response,
+            "Type \"%s\": ",
+            request.c_str());;
         if (retval != PAM_SUCCESS) {
             return retval;
         }
 
-        if (!resp || !resp[0].resp) {
+        if (!response) {
             return PAM_AUTH_ERR;
         }
 
-        LOG("Request was \"%s\", answer is \"%s\"\n", 
-            request.c_str(), resp[0].resp);
-
-        if (request != resp[0].resp) {
+        if (request != response) {
             return PAM_AUTH_ERR;
         }
     }
